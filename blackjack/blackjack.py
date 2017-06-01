eb0750602e03d90d71b9864dfb02cfbf6e75cb57
Al Burns <al.burns.email@gmail.com>
2017-06-01 15:31:14 -0700

    doing small changes

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,15 +1,17 @@
 import random
 
-
 # Create a 'card' object
 # The object should have suit_num(0-3, alphabetical), rank_num(1-13), value, suit(string), rank(string)
 # {{
 class Card(object):
 	def __init__(self, suit_num, rank_num):
-		# Organize infor based on suit number, rank number {{
+
+
+
+		# Organize information based on suit number, rank number {{
 		self.suit_num = suit_num
 		self.rank_num = rank_num
-		if self.rank_num == 0:
+		if self.rank_num == 1:
 			self.rank = 'ace'
 		elif self.rank_num > 10:
 			self.value = 10
@@ -26,6 +28,7 @@ class Card(object):
 			self.rank = str(self.rank_num)
 		# }}
 
+
 		# Make suit strings out of suit numbers {{
 		if suit_num == 0:
 			self.suit = 'clubs'
@@ -39,6 +42,7 @@ class Card(object):
 			return ValueError
 		# }}
 
+
 	def __str__(self):
 		return "The {} of {}.".format(self.rank, self.suit)
 # }}
@@ -48,6 +52,9 @@ class Card(object):
 # This should be a full set of cards. I can remove cards into a hand, and then into a discard pile.
 # {{
 class Deck(object):
+
+
+
 	# Build a deck of 4 sets of 13, and build an empty discard pile {{
 	def __init__(self):
 		self._mydeck = []
@@ -56,45 +63,78 @@ class Deck(object):
 			for j in range(1,14):
 				self._mydeck.append(Card(i,j))
 		for i in range(52):
-			print(len(self._mydeck))
 			print(self._mydeck[i])
 	# }}
 
 
 	# Return the deck as a list {{
 	def __str__(self):
-		return self._mydeck
+		deckstring = ''
+		for i in self._mydeck:
+			deckstring += str(i)
+		return deckstring
 	# }}
 	
 
 	# Shuffles the deck {{
 	def shuffle(self):
 		random.shuffle(self._mydeck)
-		return
+		return 'Shuffled!'
 	# }}
+
 
 	# Cuts the deck {{
 	def cut(self, num):
 		tempdeck = []
-		tempdeck.extend(self._mydeck[num, 52])
-		tempdeck.extend(self._mydeck[0, num])
+		tempdeck.extend(self._mydeck[num: 52])
+		tempdeck.extend(self._mydeck[0: num])
 		self._mydeck = tempdeck
 		return
 	# }}
+
+
+
 	# Draws one card from the list {{
 	def draw_one(self):
-		return self._mydeck[0].pop
+		return self._mydeck.pop(0)
 	# }}
+
+
+
 	# Puts the inputed card into the discard pile {{
 	def discard_one(self, mycard):
-		self.mydeck.append(mycard)
+		self._mydiscard.append(mycard)
 		return
 	# }}
+
+
+
 	# Adds the discard pile back into the deck, and reshuffles it {{
 	def shuffle_discard(self):
 		self._mydeck.extend(self._mydiscard)
 		random.shuffle(self._mydeck)
+		self._mydiscard = []
 	# }}
+
+
+	# Prints the deck {{
+	def print_deck(self):
+		for i in self._mydeck:
+			print(str(i))
+	# }}
+
+
+	# Prints one specific card {{
+	def print_card(self, card_num):
+		print(str(self._mydeck[card_num]))
+	# }}
+
+
+	# Prints the discard pile {{
+	def print_discard(self):
+		for i in self._mydiscard:
+			print(str(i))
+
 # }}
 
 
@@ -104,21 +144,57 @@ class Deck(object):
 # Cards collected from the deck.
 # {{
 class Hand(object):
+	
+	# Initiates an empty hand {{
 	def __init__(self):
-		self.my_hand = []
+		self._my_hand = []
 		return
+	# }}
+
+
+	# Outputs a string of card string outputs {{
 	def __str__(self):
-		return
+		self._my_hand_str = ''
+		for i in self._my_hand:
+			self._my_hand_str += str(i)
+		return self._my_hand_str
+	# }}
+
+
+	# Draw one from the deck into the hand {{
 	def draw_one(self, my_card):
-		self.my_hand.append(my_card):
+		self._my_hand.append(my_card)
 		return
+	# }}
+
+
+	# Show the full hand {{
 	def show_hand(self):
-		return self.my_hand
-	#def 
+		for i in self._my_hand:
+			print(str(i))
+	# }}
+
+
+	# Prints one specific card {{
+	def output_card(self, card_num):
+		print(str(self._my_hand[card_num]))
+	# }}
+
+
+	# Discard a card {{
+	def discard_one(self):
+		return self._my_hand.pop(0)
+	# }}
+
+
+	# Discard all cards {{
+	def discard_all(self):
+		_self.discard = []
+		for i in range(len(self._my_hand)):
+			_self.discard.append(self._my_hand.pop(i))
+		return _self.discard
+	# }}
 # }}
-
-
-
 
 
 #In the game module, implement top-level functions that:
--- blackjack/main.py
+++ blackjack/main.py
@@ -1,5 +1,31 @@
 import blackjack
-newdeck = blackjack.Deck()
-newdeck.shuffle()
+
+
+# set switches {{
+running = 1
+new_game = 1
+# }}
+
+def setup():
+	main_deck = Deck()
+	main_deck.shuffle()
+	dealer_hand = Hand()
+	player_hand = Hand()
+
+def play_hand():
+	
+
+
+while running:
+	# Establish the game if it's a new game
+	if new_game = 1:
+		setup()
+		new_game = 0
+	play_hand()
+	
+
+
+main_deck = blackjack.Deck()
+main_deck.shuffle()
 
 print(newdeck)
